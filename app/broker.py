aaf7d673850cf7cfea4d830ea0a397a31f0c27ea
Aishwarya <Aishwarya_Pradeep_Kumar@mckinsey.com>
2025-01-31 17:25:26 -0500

    network correlation id

--- app/broker.py
+++ app/broker.py
@@ -1,14 +1,14 @@
 import socket
 import struct
 
-CORRELATION_ID=7
 
 def create_broker():
     kafka_broker = socket.create_server(address=("localhost", 9092), family=socket.AF_INET,reuse_port=True)
     return kafka_broker
 
-def create_response(message_body, correlation_id):
-    message_body = message_body.encode('utf-8')
+def create_response(message_body):
+    correlation_id = struct.unpack('>iilss',message_body)
+    print(f'correlation_id: {correlation_id}')
     header = struct.pack('>i',correlation_id)
     full_message = message_body + header
     client_message = struct.pack('>i', len(full_message)) + full_message
@@ -17,9 +17,10 @@ def create_response(message_body, correlation_id):
 
 def send_response(client):
     try:
-        client_message = client.recv(1024).decode('utf-8')
+        print(f'reading message')
+        client_message = client.recv(1024)
         print(f'client message: {client_message}')
-        response = create_response("", 7)
+        response = create_response(client_message)
         client.send(response)
     except Exception as ex:
         print(f'Error occured: {ex}')
