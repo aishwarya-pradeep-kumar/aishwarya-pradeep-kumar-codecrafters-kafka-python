367560bdd22ebdf88b3a3b16dd7e09b09be18a41
Aishwarya <Aishwarya_Pradeep_Kumar@mckinsey.com>
2025-02-05 16:31:58 -0500

    network correlation id

--- app/broker.py
+++ app/broker.py
@@ -7,17 +7,15 @@ def create_broker():
     return kafka_broker
 
 def create_response(message_body):
-    message_size = struct.unpack(">i", message_body[:4])[0]
-    print(f'message_size: {message_size}')
-    request_api_key = struct.unpack(">h", message_body[4:6])[0]
-    print(f'request_api_key: {request_api_key}')
-    request_api_version = message_body[6:8]
-    print(f'request_api_version: {struct.unpack(">h",request_api_version)[0]}')
+    # message_size = struct.unpack(">i", message_body[:4])[0]
+    # print(f'message_size: {message_size}')
+    # request_api_key = struct.unpack(">h", message_body[4:6])[0]
+    # print(f'request_api_key: {request_api_key}')
+    # request_api_version = message_body[6:8]
+    # print(f'request_api_version: {struct.unpack(">h",request_api_version)[0]}')
     correlation_id = message_body[8:12]
     print(f'correlation_id: {struct.unpack(">i",correlation_id)[0]}')
-    correlation_id = struct.unpack(">i",correlation_id)[0]
-
-    header = struct.pack('>i',correlation_id)
+    header = struct.pack('>i',struct.unpack(">i",correlation_id)[0])
     full_message = message_body + header
     client_message = struct.pack('>i', len(full_message)) + full_message
     return client_message
