b2b157cd431bbf3484f9e8723001546c282798ca
Aishwarya <Aishwarya_Pradeep_Kumar@mckinsey.com>
2025-02-11 16:04:03 -0500

    apiversion check

--- app/api_versions.py
+++ app/api_versions.py
@@ -2,6 +2,7 @@ import struct
 
 def check_api_version(request_api_version):
     request_api_key = struct.unpack(">h", request_api_version[6:8])[0]
+    print(f'request_api_key: {request_api_version}')
     if 0<= request_api_key <=4:
         return True
     else:
