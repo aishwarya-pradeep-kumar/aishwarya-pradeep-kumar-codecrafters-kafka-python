9c006f5900502b45d6cb0fbbeb8b221f20ac8b9d
Aishwarya <Aishwarya_Pradeep_Kumar@mckinsey.com>
2025-02-11 16:08:08 -0500

    apiversion check

--- app/broker.py
+++ app/broker.py
@@ -16,7 +16,7 @@ def create_response(message_body):
     # print(f'request_api_version: {struct.unpack(">h",request_api_version)[0]}')
     correlation_id = message_body[8:12]
     if api_versions.check_api_version(message_body):
-        client_message =  + correlation_id + struct.pack('>h', 35)
+        client_message = correlation_id + struct.pack('>h', 35)
     else:
         client_message = correlation_id
     return struct.pack('>i', len(client_message))+client_message
